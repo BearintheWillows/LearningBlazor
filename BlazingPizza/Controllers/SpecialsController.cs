6dd5610f455417eaa9c4b7bef69930201bcc6911
BearintheWillows <sjwillows91@outlook.com>
2022-10-17 20:57:40 +0100

    Added Blazing Pizza - End Unit 7

---
+++ BlazingPizza/Controllers/SpecialsController.cs
@@ -0,0 +1,17 @@
+﻿using BlazingPizza.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Controllers;
+[Route( "api/[controller]" )]
+[ApiController]
+public class SpecialsController : Controller
+{
+    private readonly PizzaStoreContext _db;
+
+    public SpecialsController( PizzaStoreContext db ) => _db = db;
+
+    [HttpGet]
+    public async Task<ActionResult<List<PizzaSpecial>>> GetSpecials() =>
+        ( await _db.Specials.ToListAsync() ).OrderByDescending( s => s.BasePrice ).ToList();
+}
